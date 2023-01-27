701b24f2099afeef01c753341343209855944470
JayPonsford <jayponsford@hotmail.com>
2023-01-27 18:30:06 +0000

    In progress

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+  <>
+  <routes>
+    <route path="/" element={<Layout/>}
+  </routes>
+  
+  </>
+  );
+}
+
+export default App;
