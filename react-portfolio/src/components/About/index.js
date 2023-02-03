8762de2253f3b8ce2bfb86e7c8bfe301c913a50a
JayPonsford <jayponsford@hotmail.com>
2023-02-03 16:30:48 +0000

    About me text added

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,19 +1,55 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faGithub, faHtml5, faJsSquare, faReact, faSalesforce } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000);
+    }, [])
 
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
-                    <AnimatedLetters strArray={['A', 'b', 'o', 'u', 't', ' ', 'me', 'e']}
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
                     index={15}
                     />
                 </h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit gravida rutrum quisque non tellus orci ac auctor augue. Ut placerat orci nulla pellentesque dignissim enim sit. Malesuada fames ac turpis egestas.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit gravida rutrum quisque non tellus orci ac auctor augue. Ut placerat orci nulla pellentesque dignissim enim sit. Malesuada fames ac turpis egestas.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hendrerit gravida rutrum quisque non tellus orci ac auctor augue. Ut placerat orci nulla pellentesque dignissim enim sit. Malesuada fames ac turpis egestas.</p>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faReact} color="#DD0031" />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faGithub} color="#DD0031" />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faSalesforce} color="#DD0031" />
+                    </div>
+                </div>
+            </div>
+            
             </div>
         </div>
     )
