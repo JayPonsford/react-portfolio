f80225f3ce40b3c6e9b9a343821a8fad0823eb56
JayPonsford <jayponsford@hotmail.com>
2023-02-16 12:22:48 +0000

    Testing git pages

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <>
       <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="Contact" element={<Contact />} />
-      </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="Contact" element={<Contact />} />
+        </Route>
       </Routes>
     </>
   )
