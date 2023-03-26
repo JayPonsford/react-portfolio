9f92317c6b5e5f067e234e0b8701fe39686fbf1d
JayPonsford <jayponsford@hotmail.com>
2023-03-26 20:22:05 +0100

    Removal of hash routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
