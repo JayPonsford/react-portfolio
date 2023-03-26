f99c7fc9808954934644f4b2d96421e374b27fb1
JayPonsford <jayponsford@hotmail.com>
2023-03-26 17:28:18 +0100

    Formatted index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,54 +1,56 @@
-import { Link } from 'react-router-dom';
-import './index.scss';
-import { useEffect, useState } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo';
+import { Link } from 'react-router-dom'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 import Loader from 'react-loaders'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = " Jay,".split("");
-    const jobArray = "web developer".split("");
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ' Jay,'.split('')
+  const jobArray = 'web developer'.split('')
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 4000);
-        return () => {
-            clearTimeout();
-        }
-    }, [])
- 
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => {
+      clearTimeout()
+    }
+  }, [])
 
-    return (
-        <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br/> 
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                index={15}
-                />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                index={20}
-                />
-                </h1>
-                <h2>Frontend Lover / Tech Enthusiast / Coffee Enjoyer </h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-            <Logo />
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              index={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              index={20}
+            />
+          </h1>
+          <h2>Frontend Lover / Tech Enthusiast / Coffee Enjoyer </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
-        <Loader type="pacman" />
-        </>
-    );
-
+        <Logo />
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Home  
+export default Home
