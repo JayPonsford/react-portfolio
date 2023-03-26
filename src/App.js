2d0574a81b07b840c2528a0cfb789e18bcc8234a
JayPonsford <jayponsford@hotmail.com>
2023-03-26 18:21:25 +0100

    Switched to hash routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
@@ -8,13 +8,13 @@ import Contact from './components/Contact'
 function App() {
   return (
     <>
-      <Routes>
+      <HashRouter>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="Contact" element={<Contact />} />
         </Route>
-      </Routes>
+      </HashRouter>
     </>
   )
 }
