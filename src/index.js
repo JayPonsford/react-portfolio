6b0d3c959d0ee8dd49c8a4d907bb3eca45258156
JayPonsford <jayponsford@hotmail.com>
2026-05-10 22:22:27 +0100

    Updating react version and dependencies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-    ,
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
